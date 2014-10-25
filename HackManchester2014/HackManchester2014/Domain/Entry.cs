9cb5ce6e542625394465e0f28d0a3da9a47182ba
Matt Smith <m@ttsco.de>
2014-10-25 20:11:40 +0100

    store the donation details back in the database

---
+++ HackManchester2014/HackManchester2014/Domain/Entry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JG = JustGiving.Api.Sdk.Model.Donation;
+
+namespace HackManchester2014.Domain
+{
+    public class Entry
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; private set; }
+        public string UserName { get; set; }
+
+        public string ChallengeId { get; set; }
+        public string ChallengeTitle { get; set; }
+        
+        public JG.Donation Donation { get; set; }
+
+        private Entry() { }
+        public Entry(Guid userId, string userName, Challenge challenge)
+        {
+            UserId = string.Format("users/{0}", userId);
+            UserName = userName;
+            ChallengeId = challenge.Id;
+            ChallengeTitle = challenge.Title;
+        }
+
+    }
+}

66104ecc14ae89598ef95cf6eed3c1494894fc6d
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-10-25 20:09:25 +0100

    added donation hierarchy and load them sequentially to the map

--- HackManchester2014/HackManchester2014/Map/Models/MapHomeViewModel.cs
+++ HackManchester2014/HackManchester2014/Map/Models/MapHomeViewModel.cs
@@ -1,46 +1,7 @@
 ﻿namespace HackManchester2014.Map.Models
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using GeoJSON.Net.Feature;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
-
     public class MapHomeViewModel
     {
-        public MapLocation MyLocation { get; set; }
-        public List<MapLocation> Donations { get; set; }
-
-        public string DonationsJson
-        {
-            get
-            {
-                var posts = new FeatureCollection(Donations.Select(x =>
-                {
-                    var point =
-                        new GeoJSON.Net.Geometry.Point(new GeoJSON.Net.Geometry.GeographicPosition(x.Latitude,
-                            x.Longitude));
-
-                    var featureProperties = new Dictionary<string, object>
-                    {
-                        {"title", x.Title},
-                        {"description", x.Description},
-                        {"marker-size", "large"},
-                        {"marker-color", "#6FCAC5"},
-                        {"marker-symbol", "star"}
-                    };
-                    return new Feature(point, featureProperties);
-                }).ToList());
-
-                var serializedData = JsonConvert.SerializeObject(posts, Formatting.None,
-                    new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                        NullValueHandling = NullValueHandling.Ignore
-                    });
-
-                return serializedData;
-            }
-        }
+        public MapDonation Donation { get; set; }
     }
 }
--- HackManchester2014/HackManchester2014/Map/Models/MapLocation.cs
+++ HackManchester2014/HackManchester2014/Map/Models/MapLocation.cs
@@ -2,15 +2,11 @@
 {
     public class MapLocation
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
-        public MapLocation(string title, string description, double latitude, double longitude)
+        public MapLocation(double latitude, double longitude)
         {
-            Title = title;
-            Description = description;
             Latitude = latitude;
             Longitude = longitude;
         }

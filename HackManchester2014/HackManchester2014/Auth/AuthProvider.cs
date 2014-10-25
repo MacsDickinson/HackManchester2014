df041bcca07227fec4f1baf2ed4e0567f2152c48
Ashley Izat <ashleyi100@googlemail.com>
2014-10-25 13:59:24 +0100

    added nuget package and config for auth

---
+++ HackManchester2014/HackManchester2014/Auth/AuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.SimpleAuthentication;
+
+namespace HackManchester2014.Auth
+{
+    public class AuthProvider : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

5b97a805facd7d66250c7b0a20e8ca711aef8746
Ashley Izat <ashleyi100@googlemail.com>
2014-10-26 00:44:54 +0100

    add register pages

--- HackManchester2014/HackManchester2014/Home/HomeModule.cs
+++ HackManchester2014/HackManchester2014/Home/HomeModule.cs
@@ -8,6 +8,24 @@ namespace HackManchester2014.Home
         public HomeModule(IDocumentSession documentSession)
         {
             Get["/"] = _ => Negotiate.WithView("Index");
+            Get["/register"] = _ => Response.AsRedirect("/register/1");
+            Get["/register/1"] = _ =>
+            {
+                Session["returnUrl"] = "/register/2";
+                return Negotiate.WithView("Register1");
+            };
+            Get["/register/2"] = _ =>
+            {
+                return Negotiate.WithView("Register2");
+            };
+            Get["/register/3"] = _ =>
+            {
+                return Negotiate.WithView("Register3");
+            };
+            Get["/register/4"] = _ =>
+            {
+                return Negotiate.WithView("Register4");
+            };
         }
     }
 }

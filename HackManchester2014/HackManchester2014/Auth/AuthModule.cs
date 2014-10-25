9e7265152ec55dd1863fe9520b8c1cb8601677dc
Ashley Izat <ashleyi100@googlemail.com>
2014-10-25 14:44:45 +0100

    added empty auth module

---
+++ HackManchester2014/HackManchester2014/Auth/AuthModule.cs
@@ -0,0 +1,18 @@
+namespace HackManchester2014.Home
+{
+    using System.Collections.Generic;
+    using GeoJSON.Net.Feature;
+    using Nancy;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using Raven.Client;
+    using System.Linq;
+
+    public class AuthModule : NancyModule
+    {
+        public HomeModule(IDocumentSession documentSession)
+        {
+            Get["/Login"] = _ => "Hello!";
+        }
+    }
+}

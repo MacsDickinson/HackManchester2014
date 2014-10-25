263620b438ce6969171f5a4345f0568f47fbb8a9
Ashley Izat <ashleyi100@googlemail.com>
2014-10-25 20:56:22 +0100

    add parent details to entry domain model

--- HackManchester2014/HackManchester2014/Domain/Entry.cs
+++ HackManchester2014/HackManchester2014/Domain/Entry.cs
@@ -18,13 +18,23 @@ namespace HackManchester2014.Domain
         
         public JG.Donation Donation { get; set; }
 
+        public List<string> ParentEntries { get; private set; }
+        public string ParentEntry { get { return ParentEntries.LastOrDefault(); } }
         private Entry() { }
-        public Entry(User user, Challenge challenge)
+        public Entry(User user, Challenge challenge, Entry parentEntry = null)
         {
             UserId = string.Format("users/{0}", user.Id);
             UserName = user.UserInfo.Name;
             ChallengeId = challenge.Id;
             ChallengeTitle = challenge.Title;
+            if (parentEntry != null)
+            {
+                ParentEntries = parentEntry.ParentEntries.Concat(new List<string> {parentEntry.Id}).ToList();
+            }
+            else
+            {
+                ParentEntries = new List<string>();
+            }
         }
 
     }
